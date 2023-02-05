908d07423572f7a0d474b7df4057f982e04aea6e
Otto-Pekka Taskinen <hippopotaska@gmail.com>
2023-02-05 03:46:10 +0200

    Simon says work work

--- Assets/Scripts/Minigame/SimonSays/SimonSaysMinigame.cs
+++ Assets/Scripts/Minigame/SimonSays/SimonSaysMinigame.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public enum Directions {
+public enum Direction {
     Left = 0,
     Up = 1,
     Right = 2,
@@ -17,6 +17,8 @@ public class SimonSaysMinigame : HackingBase
     [Space(5), Header("Colors")]
     public Color arrowDefault;
     public Color arrowHighlight;
+    public Color arrowPlayer;
+
     public Color lightDefault;
     public Color lightHighlight;
 
@@ -24,14 +26,11 @@ public class SimonSaysMinigame : HackingBase
     public float timeBetweenFlashes;
     public float flashTime;
 
-    private Directions[] _simonInputs;
-    private Directions[] _playerInputs;
+    private List<Direction> _simonInputs = new List<Direction>();
+    private List<Direction> _playerInputs = new List<Direction>();
     private bool _isPlayerTurn;
 
-    private void Start()
-    {
-        Initialize();
-    }
+    private void Start() => Initialize();
 
     // Update is called once per frame
     void Update()
@@ -39,24 +38,40 @@ public class SimonSaysMinigame : HackingBase
         var Inputs = GameManager.Instance.Inputs;
 
         if (Inputs.IsDown(InputHandler.InputType.MoveLeft) && _isPlayerTurn)
-        {
-            arrows[(int)Directions.Left].color = arrowHighlight;
-        }
+            PlayerInput(Direction.Left);
         if (Inputs.IsDown(InputHandler.InputType.MoveUp) && _isPlayerTurn)
-        {
-            arrows[(int)Directions.Up].color = arrowHighlight;
-        }
+            PlayerInput(Direction.Up);
         if (Inputs.IsDown(InputHandler.InputType.MoveRight) && _isPlayerTurn)
-        {
-            arrows[(int)Directions.Right].color = arrowHighlight;
-        }
+            PlayerInput(Direction.Right);
         if (Inputs.IsDown(InputHandler.InputType.MoveDown) && _isPlayerTurn)
-        {
-            arrows[(int)Directions.Down].color = arrowHighlight;
-        }
+            PlayerInput(Direction.Down);
 
         // TODO: Compare Inputs
+        if (_playerInputs == _simonInputs)
+        {
+            StartCoroutine(SimonSays());
+        }
     }
+    private void PlayerInput(Direction dir)
+    {
+        arrows[(int)dir].color = arrowPlayer;
+        _playerInputs.Add(dir);
+    }
+
+    IEnumerator SimonSays()
+    {
+        _isPlayerTurn = false;
+        foreach (Direction input in _simonInputs)
+        {
+            arrows[(int)input].color = arrowHighlight;
+            yield return new WaitForSeconds(flashTime);
+            arrows[(int)input].color = arrowDefault;
+            yield return new WaitForSeconds(timeBetweenFlashes);
+        }
+        _playerInputs.Clear();
+        _isPlayerTurn = true;
+    }
+
     public override void Cancel() => throw new System.NotImplementedException();
     public override void Finish() => throw new System.NotImplementedException();
     public override void Initialize()
@@ -66,6 +81,11 @@ public class SimonSaysMinigame : HackingBase
         foreach (var light in lights)
             light.color = lightDefault;
 
-        _isPlayerTurn = true;
+        int n = Random.Range(0, 4);
+        Direction startDir = (Direction)n;
+        _simonInputs.Add(startDir);
+
+        _isPlayerTurn = false;
+        StartCoroutine(SimonSays());
     }
 }
