d643ef64f034403aa5e18310514fc11a9d6296ba
Otto-Pekka Taskinen <hippopotaska@gmail.com>
2023-02-05 04:29:52 +0200

    Simon says it's working now

--- Assets/Scripts/Minigame/SimonSays/SimonSaysMinigame.cs
+++ Assets/Scripts/Minigame/SimonSays/SimonSaysMinigame.cs
@@ -26,54 +26,127 @@ public class SimonSaysMinigame : HackingBase
     public float timeBetweenFlashes;
     public float flashTime;
 
-    private List<Direction> _simonInputs = new List<Direction>();
-    private List<Direction> _playerInputs = new List<Direction>();
-    private bool _isPlayerTurn;
+    [SerializeField] private List<Direction> _simonInputs = new List<Direction>();
+    [SerializeField] private List<Direction> _playerInputs = new List<Direction>();
+    [SerializeField] private bool _isPlayerTurn;
+    private bool _isPressing;
+    private int _curRound = 0;
 
     private void Start() => Initialize();
 
     // Update is called once per frame
     void Update()
     {
+        if (!_isPlayerTurn)
+            return;
+
         var Inputs = GameManager.Instance.Inputs;
 
-        if (Inputs.IsDown(InputHandler.InputType.MoveLeft) && _isPlayerTurn)
+        if (Inputs.IsDown(InputHandler.InputType.MoveLeft))
             PlayerInput(Direction.Left);
-        if (Inputs.IsDown(InputHandler.InputType.MoveUp) && _isPlayerTurn)
+        if (Inputs.IsUp(InputHandler.InputType.MoveLeft))
+            PlayerRelease(Direction.Left);
+
+        if (Inputs.IsDown(InputHandler.InputType.MoveUp))
             PlayerInput(Direction.Up);
-        if (Inputs.IsDown(InputHandler.InputType.MoveRight) && _isPlayerTurn)
+        if (Inputs.IsUp(InputHandler.InputType.MoveUp))
+            PlayerRelease(Direction.Up);
+
+        if (Inputs.IsDown(InputHandler.InputType.MoveRight))
             PlayerInput(Direction.Right);
-        if (Inputs.IsDown(InputHandler.InputType.MoveDown) && _isPlayerTurn)
+        if (Inputs.IsUp(InputHandler.InputType.MoveRight))
+            PlayerRelease(Direction.Right);
+
+        if (Inputs.IsDown(InputHandler.InputType.MoveDown))
             PlayerInput(Direction.Down);
+        if (Inputs.IsUp(InputHandler.InputType.MoveDown))
+            PlayerRelease(Direction.Down);
 
         // TODO: Compare Inputs
-        if (_playerInputs == _simonInputs)
+        if (_playerInputs.Count == _simonInputs.Count && !_isPressing)
         {
-            StartCoroutine(SimonSays());
+            foreach (var arrow in arrows)
+                arrow.color = arrowDefault;
+
+            bool areIdentical = true;
+            for (int i = 0; i < _playerInputs.Count; i++)
+            {
+                if (_playerInputs[i] != _simonInputs[i])
+                {
+                    areIdentical = false;
+                    break;
+                }
+            }
+            if (areIdentical)
+                StartCoroutine(SimonSays(false));
+            else
+                Cancel();
         }
     }
     private void PlayerInput(Direction dir)
     {
         arrows[(int)dir].color = arrowPlayer;
         _playerInputs.Add(dir);
+        _isPressing = true;
+    }
+    private void PlayerRelease(Direction dir)
+    {
+        arrows[(int)dir].color = arrowDefault;
+        _isPressing = false;
     }
 
-    IEnumerator SimonSays()
+    private void NewSimonInput()
     {
+        int n = Random.Range(0, 4);
+        Direction startDir = (Direction)n;
+        _simonInputs.Add(startDir);
+    }
+
+    IEnumerator SimonSays(bool isInit)
+    {
+        if (_curRound == 3)
+        {
+            Finish();
+            yield break;
+        }
+
         _isPlayerTurn = false;
-        foreach (Direction input in _simonInputs)
+        if (isInit)
+            yield return new WaitForSeconds(0.5f);
+
+        if (!isInit)
+        {
+            NewSimonInput();
+            lights[_curRound].color = lightHighlight;
+            _curRound++;
+        }
+
+        foreach (var input in _simonInputs)
         {
             arrows[(int)input].color = arrowHighlight;
             yield return new WaitForSeconds(flashTime);
             arrows[(int)input].color = arrowDefault;
             yield return new WaitForSeconds(timeBetweenFlashes);
         }
+
         _playerInputs.Clear();
         _isPlayerTurn = true;
     }
 
-    public override void Cancel() => throw new System.NotImplementedException();
-    public override void Finish() => throw new System.NotImplementedException();
+    public override void Cancel()
+    {
+        Debug.Log("You loser!");
+        _playerInputs.Clear();
+        _simonInputs.Clear();
+        _isPlayerTurn = false;
+    }
+    public override void Finish()
+    {
+        Debug.Log("You win!");
+        _playerInputs.Clear();
+        _simonInputs.Clear();
+        _isPlayerTurn = false;
+    }
     public override void Initialize()
     {
         foreach (var arrow in arrows)
@@ -81,11 +154,9 @@ public class SimonSaysMinigame : HackingBase
         foreach (var light in lights)
             light.color = lightDefault;
 
-        int n = Random.Range(0, 4);
-        Direction startDir = (Direction)n;
-        _simonInputs.Add(startDir);
+        NewSimonInput();
 
         _isPlayerTurn = false;
-        StartCoroutine(SimonSays());
+        StartCoroutine(SimonSays(true));
     }
 }
